5ebbd7789d9e12dcf39b280c59f60cb0ddee74f2
cfinn7789 <cfinn7789@gmail.com>
2024-08-03 15:44:48 -0400

    set up app and main files

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import './App.css';
+
+import App from './App';
+import Home from './pages/Home';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Stories from './pages/Stories';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Gallery',
+        element: <Gallery />,
+      },
+      {
+        path: '/Stories',
+        element: <Stories />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
